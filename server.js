5f7da543aa4978b63a6f0d405daebbc1ea02c986
Geetesh Aheriya <141254341+GeeteshA@users.noreply.github.com>
2025-01-10 20:53:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -83,5 +83,7 @@ app.use(errorHandler);
 // Define the PORT
 const PORT = process.env.PORT || 5000;
 
+
 // Start the server
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in http://${process.env.NODE_ENV}:${PORT}`));
+
